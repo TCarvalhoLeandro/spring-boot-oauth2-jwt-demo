b8548272928335043936508b0de047bca85dfde5
TCarvalhoLeandro <trairicarvalho@gmail.com>
2026-05-20 11:38:24 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner{
 
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+//		System.out.println(passwordEncoder.encode("123456"));
+//		
+//		boolean result = passwordEncoder.matches("1234566", "$2a$10$nDej4yqSDZUQN5A79tEBluwfKakjU9c2X83p.EYh9OpdFFH/6CuFi");
+//		System.out.println("Result = " + result);
+		
+		
 	}
 
 }
--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$nDej4yqSDZUQN5A79tEBluwfKakjU9c2X83p.EYh9OpdFFH/6CuFi');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$nDej4yqSDZUQN5A79tEBluwfKakjU9c2X83p.EYh9OpdFFH/6CuFi');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
